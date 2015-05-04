db3aeb1104bd70a1127a92f91860336b00f7d86f
Ikumi-otani <matelediter@gmail.com>
2015-05-04 14:34:16 +0000

    first commit

---
+++ src/app.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class MainApp < Sinatra::Base
+  # Sinatra Auto Reload
+  configure :development do
+    register Sinatra::Reloader
+  end
+  get '/' do
+    'Hello, World!'
+  end
+end
